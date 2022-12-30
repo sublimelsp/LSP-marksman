e1e09af2dffdad9826a32a74341e72fe2955a724
Jack Cherng <jfcherng@gmail.com>
2022-12-30 19:08:04 +0800

    chore: update marksman to version 2022-12-28 (#11)

--- plugin.py
+++ plugin.py
@@ -6,7 +6,7 @@ import sublime
 import urllib.request
 
 
-MARKSMAN_TAG = '2022-12-23'
+MARKSMAN_TAG = '2022-12-28'
 MARKSMAN_RELEASES_BASE = 'https://github.com/artempyanykh/marksman/releases/download/{tag}/{platform}'
 USER_AGENT = 'Sublime Text LSP'
 
